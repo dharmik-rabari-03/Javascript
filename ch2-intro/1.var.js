9fe335184326eb42932d19b7871cda95c1032d8e
Dharmik rabari <dharmik161616@gmail.com>
2026-01-28 18:06:26 +0530

    Add files via upload

---
+++ ch2-intro/1.var.js
@@ -0,0 +1,31 @@
+
+var greet="good morning"
+
+
+function Name(){
+
+    var name="Dharmik"
+    console.log(name)
+
+}
+
+console.log(greet)
+
+greet="good evening"
+console.log(greet)
+
+var greet="good night"
+console.log(greet)
+
+
+Name();
+
+
+
+
+
+
+
+
+
+
---
+++ ch2-intro/3.const.js
@@ -0,0 +1,34 @@
+const a=10
+// a=20 , can't cahnge value 
+
+
+const b=20
+// b=30
+
+
+
+console.log(a+b)
+
+
+//Re-intialiazation is not possible
+
+const greet="hi"
+// greet="hello"
+
+console.log(greet)
+
+
+//Re-Declare is not Possible
+
+
+const Name="Dharmik"
+
+// const Name="Dharmik Ragiya"
+
+
+
+
+
+
+
+
