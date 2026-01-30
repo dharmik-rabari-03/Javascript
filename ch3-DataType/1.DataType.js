3b41cd46d8e28f628abc89bd27cfccdf226577dd
Dharmik rabari <dharmik161616@gmail.com>
2026-01-30 14:51:19 +0530

    Update 1.DataType.js

--- ch3-DataType/1.DataType.js
+++ ch3-DataType/1.DataType.js
@@ -1,11 +1,28 @@
 //premitive data type
 
+
+
 //1.string
 
 let string = "dharmik";
 
 console.log(string);
 console.log(typeof string);
+
+// var string2 = "dharmik ragiya";
+
+// console.log(string2);
+// console.log(typeof string2);
+
+// const string3 = "ragiya dharmik";
+
+// console.log(string3);
+// console.log(typeof string3);
+
+
+
+
+
 
 //2.bigint
 
@@ -14,6 +31,21 @@ let BigInt = 7573068252n;
 console.log(BigInt);
 console.log(typeof BigInt);
 
+
+// var BigInt2 = 6468413524687n;
+
+// console.log(BigInt2);
+// console.log(typeof BigInt2);
+
+// let BigInt3 = 1526376316n;
+
+// console.log(BigInt3);
+// console.log(typeof BigInt3);
+
+
+
+
+
 //3.number
 
 let number = 100;
@@ -21,10 +53,35 @@ let number = 100;
 console.log(number);
 console.log(typeof number);
 
+// let number2 = 400;
+
+// console.log(number2);
+// console.log(typeof number2);
+
+
+// let number3 = 100;
+
+// console.log(number3);
+// console.log(typeof number3);
+
+
+
+
 //4.undefine
 
 let a;
 console.log(typeof a);
+
+// let b;
+// console.log(typeof b);
+
+// let C;
+// console.log(typeof C);
+
+
+
+
+
 
 //5.Boolean
 
@@ -33,6 +90,22 @@ let D = true;
 console.log(D);
 console.log(typeof D);
 
+
+// let E = false;
+
+// console.log(E);
+// console.log(typeof E);
+
+
+// let F = true;
+
+// console.log(F);
+// console.log(typeof F);
+
+
+
+
+
 //6.symbol
 
 let c = Symbol(10);
@@ -40,15 +113,50 @@ let d = Symbol(10);
 
 console.log(c == d);
 
+
+
+// let S1 = Symbol(80);
+// let S2 = Symbol(90);
+
+// console.log(S1 == S2);
+
+
+// let S3 = Symbol(90);
+// let S4 = Symbol(90);
+
+// console.log(S3 == S4);
+
+
+
+
+
 //7.null
 
 let NULL = null;
 
 console.log(typeof NULL);
 
+
+
+// let NULL1 = null;
+
+// console.log(typeof NULL1);
+
+
+// let NULL2 = null;
+
+// console.log(typeof NULL2);
+
+
+
+
+
+
+
 //non-premitive data types
 
 //8.object
+
 
 const Student = { NAME: "Dharmik", Age: "18" };
 
@@ -58,3 +166,26 @@ console.log(typeof Student);
 Student.NAME="prince"
 
 console.log(Student)
+
+
+
+
+
+// const Student1 = { NAME: "ankit", Age: "20" };
+
+// console.log(Student1);
+// console.log(typeof Student1);
+
+// Student1.NAME="dharmik"
+
+// console.log(Student1)
+
+
+// const Student2 = { NAME: "prince", Age: "18" };
+
+// console.log(Student2);
+// console.log(typeof Student2);
+
+// Student2.NAME="dharmik"
+
+// console.log(Student2)
