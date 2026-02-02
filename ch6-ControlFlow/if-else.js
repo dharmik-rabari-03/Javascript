b725a26159e5cac0ca07fd21bf734f8b332cdea7
Dharmik rabari <dharmik161616@gmail.com>
2026-02-02 22:25:07 +0530

    Add files via upload

---
+++ ch6-ControlFlow/if-else.js
@@ -0,0 +1,16 @@
+//IF 
+
+let Age=true;
+
+if(Age){
+    console.log("You Are Eligible")
+}
+
+//IF-ELSE
+
+let age=18;
+
+if(age>=18){
+    console.log("you Are eligible for vote")
+}
+else("You are not eligible for vote")
