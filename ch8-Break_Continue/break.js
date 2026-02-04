70b48ad9dbd58e926f62a08c6b34e6b8f48a0589
Dharmik rabari <dharmik161616@gmail.com>
2026-02-04 14:42:25 +0530

    Update break.js

--- ch8-Break_Continue/break.js
+++ ch8-Break_Continue/break.js
@@ -1,3 +1,6 @@
+//Example 1
+console.log("\nExample 2\n")
+
 let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for (let i = 0; i <= num.length; i++) {
@@ -8,3 +11,17 @@ for (let i = 0; i <= num.length; i++) {
 
 }
     console.log("Found 5 ! stop ")
+
+//Example 2
+
+console.log("\nExample 2\n")
+
+let Fruit=["apple", "banana", "mango", "orange", "grapes"];
+
+for(let i=0;i<=Fruit.length;i++){
+    if(Fruit[i] === "mango"){
+        console.log("mango found ! stop");
+        break;
+    }
+    console.log(Fruit[i]);
+}
