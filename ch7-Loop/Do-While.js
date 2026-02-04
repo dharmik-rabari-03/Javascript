2a6b3b393dd5df4d388aaea1c078ca100d51bd70
Dharmik rabari <dharmik161616@gmail.com>
2026-02-04 14:40:58 +0530

    Update Do-While.js

--- ch7-Loop/Do-While.js
+++ ch7-Loop/Do-While.js
@@ -1,7 +1,20 @@
+//Exqample 1
+
 let num = 1;
 
 do {
-    console.log("this will print At least one time")
-    num++;
+  console.log("this will print At least one time");
+  num++;
+} while (num > 10);
 
-} while (num > 10)
+
+
+//Example 2
+
+let Num = 10;
+let i=1;
+
+do {
+  console.log("This will print ");
+ 
+} while (i>Num);
