f1212edc72b92560a818cfafc305dd7eb932b492
Dharmik rabari <dharmik161616@gmail.com>
2026-02-05 15:12:37 +0530

    Add files via upload

---
+++ ch9-Range-Program/grade/grade.js
@@ -0,0 +1,24 @@
+
+document.getElementById("form").addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    let marks = document.getElementById("mark").value;
+    let result = document.getElementById("result");
+
+
+    if (marks >= 90) {
+        result.innerText = `Achive A grade`;
+    } 
+    else if (marks >= 80) {
+        result.innerText = `Achive B grade`;
+    } 
+    else if (marks >= 70) {
+        result.innerText = `Achive C grade`;
+    } 
+    else if (marks >= 35) {
+        result.innerText = `Achive D grade`;
+    } 
+    else {
+        result.innerText = `Fail`;
+    }
+});
