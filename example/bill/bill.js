ed27d744b57267e34b9cbfcbd90dafd1c46f2304
Dharmik rabari <dharmik161616@gmail.com>
2026-02-05 12:12:41 +0530

    Add files via upload

---
+++ example/bill/bill.js
@@ -0,0 +1,25 @@
+document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+    let unit = document.getElementById("unit").value;
+    let result = document.getElementById("result");
+    let bill;
+
+    if (unit <= 100) {
+        bill = unit * 10
+    } else if (unit <= 200) {
+        bill = 100 * 10 + (unit - 100) * 20
+    }
+    else if (unit <= 300) {
+        bill = 100 * 10 + 100 * 20 + (unit - 200) * 30
+    }
+    else if (unit <= 400) {
+        bill = 100 * 10 + 100 * 20 + 100 * 30 + (unit - 300) * 40
+    }
+    else{
+        result.innerText=`plz enter unit under 400`
+    }
+
+    result.innerText = `total bill ` + bill;
+})
