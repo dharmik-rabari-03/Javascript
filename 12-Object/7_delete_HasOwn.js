913b996e64b40b650a9e18bc4b5c45f55c5597b5
Dharmik rabari <dharmik161616@gmail.com>
2026-02-24 10:52:40 +0530

    Update 7_delete_HasOwn.js

--- 12-Object/7_delete_HasOwn.js
+++ 12-Object/7_delete_HasOwn.js
@@ -1,3 +1,5 @@
+//example 1
+
 const student={
     name:"dharmik",
     age:"18",
@@ -8,7 +10,6 @@ const student={
 delete student.age;
 
 console.log(student)
-
 
 //HasOwnProperties
 
@@ -21,3 +22,24 @@ console.log(Object.keys(student))
 //value
 
 console.log(Object.values(student))
+
+//example 2
+
+
+const employee={
+    name:"dharmik",
+    age:"18",
+    salary:"1,00,000"
+}
+console.log(employee)
+
+delete employee.salary;
+
+console.log(employee)
+
+
+console.log(employee.hasOwnProperty("salary"))
+
+console.log(Object.keys(employee))
+
+console.log(Object.values(employee))
