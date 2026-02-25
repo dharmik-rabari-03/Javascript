b0ed585ec063ce5e4a96f2ebbff27382a1d541ca
Dharmik rabari <dharmik161616@gmail.com>
2026-02-25 14:44:45 +0530

    Add files via upload

---
+++ 13-Array/1.Array.js
@@ -0,0 +1,32 @@
+//array is store multiple value in single variable 
+
+//litral array
+
+//example 1
+
+const fruit=["mango","pineapple","chery","watermelon","grapes"];
+
+console.log(fruit)
+
+//example 2
+
+const student=["dharmik","ankit","prince","amit"]
+console.log(student)
+
+
+
+//constructor
+
+//example 1
+
+const vegetables= new Array("pototo","tomato","carrots","broccoli")
+
+
+console.log(typeof vegetables)  
+console.log(vegetables)
+
+
+//example 2
+
+const employee=["1.dharmik","2.ankit","3.prince","4.amit"]
+console.log(employee)
