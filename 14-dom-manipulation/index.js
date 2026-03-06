5dbfbfc47401251397a4eff1dbfda805eb22c464
Dharmik rabari <dharmik161616@gmail.com>
2026-03-06 22:30:12 +0530

    Add files via upload

--- 14-dom-manipulation/index.js
+++ 14-dom-manipulation/index.js
@@ -1,23 +1,66 @@
+//using id
+
 document.getElementById("btn").addEventListener("click", () => {
 
   let text = document.getElementById("id");
 
-  text.innerHTML="<h3>text changed</h3>"
+  text.innerHTML = "<h3>text changed</h3>"
 
-    text.style.color="red"
-    
+  text.style.color = "red"
+
 });
 
 
-
-document.getElementsByClassName("btn")[0].addEventListener("click",()=>{
-
-  let result =document.getElementsByClassName("class1")[0]
-
-  result.innerHTML="<h3>text changed</h3>"
+//using className
 
 
+document.getElementsByClassName("btn")[0].addEventListener("click", () => {
 
-  result.style.color="green"
+  let result = document.getElementsByClassName("class1")[0]
+
+  result.innerHTML = "<h3>text changed</h3>"
+
+
+
+  result.style.color = "green"
+
+})
+
+
+//querySelector
+
+let result2 = document.querySelector("#btn2")
+
+result2.addEventListener("click", () => {
+
+
+  result2.innerHTML = `hello`
+
+})
+
+
+//querySelectorAll
+
+
+let resultP = document.querySelectorAll(".p")[0]
+let resultP2 = document.querySelectorAll(".p")[1]
+let resultP3 = document.querySelectorAll(".p")[2]
+
+resultP.addEventListener("click", () => {
+
+  resultP.innerHTML = `hello`
+  resultP2.innerHTML = `hello`
+  resultP3.innerHTML = `hello`
+
+})
+
+//by tag
+
+let tag = document.getElementsByTagName("h2")[0]
+
+
+tag.addEventListener("click", () => {
+
+  tag.innerHTML = `text changed`
 
 })
