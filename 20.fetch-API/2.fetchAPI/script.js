2c9965252f4d4417240ec2ed49730e4ee3ec3c80
Dharmik rabari <dharmik161616@gmail.com>
2026-03-27 11:07:04 +0530

    Add files via upload

---
+++ 20.fetch-API/2.fetchAPI/script.js
@@ -0,0 +1,24 @@
+document.getElementById("btn").addEventListener("click",
+    async function imges() {
+        
+        const img=document.getElementById("img")
+
+
+        try {
+
+            const api= await fetch("https://dog.ceo/api/breeds/image/random")
+
+
+
+            const data= await api.json()
+
+            img.src = data.message;
+
+            
+          
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+)
