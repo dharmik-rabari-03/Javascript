a2de5fce88478287129a6127ed00f2ab43288194
Dharmik rabari <dharmik161616@gmail.com>
2026-04-08 15:02:08 +0530

    Add files via upload

---
+++ array practice/1.array.js
@@ -0,0 +1,14 @@
+//array is store multiple value in single variable
+
+//litral synrax
+
+let ArrayName=[1,2,3,4,5,6,7,8,9,10]
+
+console.log("this is litral syntax : ",ArrayName)
+
+
+//constructor syntax
+
+let ArrayName2= new Array("kim jong","putin","narendra","biden")
+
+console.log("this is constructor : ",ArrayName2)
---
+++ array practice/2.array.js
@@ -0,0 +1,24 @@
+//accesing Array
+
+let fruit=["mango","banana","apple","chery","guava","pineapple"];
+
+//access first element 
+
+console.log("First element : ",fruit[0])
+
+//access random element
+
+console.log("random element : ",fruit[4])
+
+//access last element
+
+console.log("last element : ",fruit[fruit.length-1])
+
+
+//modyfying
+
+console.log("before modifying  element : ",fruit[0])
+
+fruit[0]="strawberry"
+
+console.log("after modifying element : ",fruit[0])
