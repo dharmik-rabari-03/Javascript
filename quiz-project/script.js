2d8a6905551ff0c844a1d4e9d6d2b9d9e9efa476
Dharmik rabari <dharmik161616@gmail.com>
2026-04-13 13:10:05 +0530

    Add files via upload

--- quiz-project/script.js
+++ quiz-project/script.js
@@ -1,80 +1,80 @@
 let Questions = [
-  {
-    id: 1,
-    question: "What does HTML stand for?",
-    options: [
-      "Hyper Trainer Marking Language",
-      "Hyper Text Marketing Language",
-      "Hyper Text Markup Language",
-      "Hyper Tool Multi Language",
-    ],
-    correctIndex: 2,
-  },
-  {
-    id: 2,
-    question: "Which CSS property is used to change text color?",
-    options: ["font-color", "text-color", "color", "background-color"],
-    correctIndex: 2,
-  },
-  {
-    id: 3,
-    question: "Which JavaScript method is used to select an element by ID?",
-    options: [
-      "getElementByClass()",
-      "getElementById()",
-      "querySelectorAll()",
-      "getId()",
-    ],
-    correctIndex: 1,
-  },
-  {
-    id: 4,
-    question: "Which HTML tag is used to create a hyperlink?",
-    options: ["<link>", "<a>", "<href>", "<url>"],
-    correctIndex: 1,
-  },
-  {
-    id: 5,
-    question: "Which CSS property controls spacing between elements?",
-    options: ["padding", "margin", "spacing", "border"],
-    correctIndex: 1,
-  },
-  {
-    id: 6,
-    question: "Which of the following is a JavaScript framework?",
-    options: ["Django", "Laravel", "React", "Flask"],
-    correctIndex: 2,
-  },
-  {
-    id: 7,
-    question: "Which HTML tag is used to display an image?",
-    options: ["<image>", "<img>", "<src>", "<pic>"],
-    correctIndex: 1,
-  },
-  {
-    id: 8,
-    question: "What does CSS stand for?",
-    options: [
-      "Creative Style Sheets",
-      "Cascading Style Sheets",
-      "Colorful Style Sheets",
-      "Computer Style Sheets",
-    ],
-    correctIndex: 1,
-  },
-  {
-    id: 9,
-    question:
-      "Which method is used to add an element at the end of an array in JavaScript?",
-    options: ["push()", "pop()", "shift()", "unshift()"],
-    correctIndex: 0,
-  },
-  {
-    id: 10,
-    question: "Which HTML element is used for the largest heading?",
-    options: ["<heading>", "<h6>", "<h1>", "<head>"],
-    correctIndex: 2,
-  },
+    {
+        id: 1,
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Trainer Marking Language",
+            "Hyper Text Marketing Language",
+            "Hyper Text Markup Language",
+            "Hyper Tool Multi Language",
+        ],
+        correctIndex: 2,
+    },
+    {
+        id: 2,
+        question: "Which CSS property is used to change text color?",
+        options: ["font-color", "text-color", "color", "background-color"],
+        correctIndex: 2,
+    },
+    {
+        id: 3,
+        question: "Which JavaScript method is used to select an element by ID?",
+        options: [
+            "getElementByClass()",
+            "getElementById()",
+            "querySelectorAll()",
+            "getId()",
+        ],
+        correctIndex: 1,
+    },
+    {
+        id: 4,
+        question: "Which HTML tag is used to create a hyperlink?",
+        options: ["<link>", "<a>", "<href>", "<url>"],
+        correctIndex: 1,
+    },
+    {
+        id: 5,
+        question: "Which CSS property controls spacing between elements?",
+        options: ["padding", "margin", "spacing", "border"],
+        correctIndex: 1,
+    },
+    {
+        id: 6,
+        question: "Which of the following is a JavaScript framework?",
+        options: ["Django", "Laravel", "React", "Flask"],
+        correctIndex: 2,
+    },
+    {
+        id: 7,
+        question: "Which HTML tag is used to display an image?",
+        options: ["<image>", "<img>", "<src>", "<pic>"],
+        correctIndex: 1,
+    },
+    {
+        id: 8,
+        question: "What does CSS stand for?",
+        options: [
+            "Creative Style Sheets",
+            "Cascading Style Sheets",
+            "Colorful Style Sheets",
+            "Computer Style Sheets",
+        ],
+        correctIndex: 1,
+    },
+    {
+        id: 9,
+        question:
+            "Which method is used to add an element at the end of an array in JavaScript?",
+        options: ["push()", "pop()", "shift()", "unshift()"],
+        correctIndex: 0,
+    },
+    {
+        id: 10,
+        question: "Which HTML element is used for the largest heading?",
+        options: ["<heading>", "<h6>", "<h1>", "<head>"],
+        correctIndex: 2,
+    },
 ];
 
 let qnsIndexHTML = document.getElementById("qnsIndexHTML");
@@ -84,58 +84,111 @@ let nextbtn = document.getElementById("nextbtn");
 let qnsIndex = 0;
 let score = 0;
 
-let selectedAnswer=null;
+let selectedAnswer = null;
 
-  let qns = document.getElementById("qns");
-  let optionsDiv = document.getElementById("options");
+let qns = document.getElementById("qns");
+let optionsDiv = document.getElementById("options");
 
 function loadQns() {
+    selectedAnswer = null;
 
 
-  qns.innerHTML = Questions[qnsIndex].question;
-  optionsDiv.innerHTML = "";
+    qns.innerHTML = Questions[qnsIndex].question;
+    optionsDiv.innerHTML = "";
 
-  Questions[qnsIndex].options.forEach((opt, index) => {
-    let div = document.createElement("div");
-    div.classList.add("col-6");
+    Questions[qnsIndex].options.forEach((opt, index) => {
+        let div = document.createElement("div");
+        div.classList.add("col-6");
 
-    let btn = document.createElement("button");
-    btn.innerText = opt;
+        let btn = document.createElement("button");
+        btn.innerText = opt;
 
-    btn.classList.add("p-2", "btn", "btn-option", "w-100");
-    btn.style.minHeight = "70px";
+        btn.classList.add("p-2", "btn", "btn-option", "w-100");
+        btn.style.minHeight = "70px";
 
-    btn.addEventListener("click", () => {
-      selectedAnswer = index;
+        btn.addEventListener("click", () => {
+            selectedAnswer = index;
 
-      nextqns();
+            nextqns();
+        });
+
+        div.appendChild(btn);
+        optionsDiv.appendChild(div);
     });
-
-    div.appendChild(btn);
-    optionsDiv.appendChild(div);
-  });
 }
 
 loadQns();
 
+let userAnswers = [];
+
 function nextqns() {
 
-   if (selectedAnswer === Questions[qnsIndex].correctIndex) {
-    score++;
-  }
+    userAnswers.push(
+        selectedAnswer !== null ? selectedAnswer : null
+    );
 
-  qnsIndex++;
-  if (qnsIndex < Questions.length) {
-    qnsIndexHTML.innerHTML = `question: ${qnsIndex + 1}/${Questions.length}`;
-    
-    loadQns();
-  } else {
-    showResult();
-  }
+    if (selectedAnswer === Questions[qnsIndex].correctIndex) {
+        score++;
+    }
+
+    qnsIndex++;
+    if (qnsIndex < Questions.length) {
+        qnsIndexHTML.innerHTML = `question: ${qnsIndex + 1}/${Questions.length}`;
+
+        loadQns();
+    } else {
+        showResult();
+    }
+
 }
 
 
 function showResult() {
-  qns.innerHTML =`Your Score: ${score}/${Questions.length}`;
-  options.innerHTML = "";
+    nextbtn.classList.add("d-none");
+
+    qns.innerHTML = `Your Score: ${score}/${Questions.length}`;
+    optionsDiv.innerHTML = "";
+
+
+    let qnsHTML = document.getElementById("qnsResult")
+    qnsHTML.innerHTML = "";
+
+    Questions.forEach((qns, index) => {
+
+
+
+        let userAns =
+            userAnswers[index] !== null
+                ? qns.options[userAnswers[index]]
+                : "Not Attempted";
+
+        let container = document.createElement("div");
+        container.classList.add("text-start")
+
+        let qnsShow = document.createElement("h3")
+        qnsShow.innerText = `${index + 1}.${Questions[index].question}`
+        qnsShow.classList.add("mt-5", "mb-3")
+
+        let correctAnswer = document.createElement("p")
+        correctAnswer.innerText = `correct Ans : ${Questions[index].options[Questions[index].correctIndex]}`
+        correctAnswer.classList.add("text-success", "mt-4")
+
+        let selectedAnswer = document.createElement("p")
+        selectedAnswer.innerText = `your Ans : ${userAns}`
+        if (userAnswers[index] === qns.correctIndex) {
+            selectedAnswer.classList.add("text-success");
+        } else {
+            selectedAnswer.classList.add("text-danger");
+        }
+
+
+        container.appendChild(qnsShow);
+        container.appendChild(correctAnswer);
+        container.appendChild(selectedAnswer);
+
+        qnsHTML.appendChild(container);
+
+
+    })
+
 }
