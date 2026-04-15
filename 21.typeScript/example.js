a6902351273b7998db4a30c4b7877bab88621fac
Dharmik rabari <dharmik161616@gmail.com>
2026-04-15 10:39:18 +0530

    Add files via upload

---
+++ 21.typeScript/example.js
@@ -0,0 +1,7 @@
+"use strict";
+let num1 = 10;
+let num2 = 20;
+function add(a, b) {
+    return a + b;
+}
+console.log(add(12, 12));
---
+++ 21.typeScript/example.ts
@@ -0,0 +1,9 @@
+
+let num1:number=10;
+let num2:number=20;
+
+function add(a:number,b:number):number{
+    return a+b;
+}
+
+console.log(add(12,12))
