2fe87bdad819f95ebff80d9144dad51095877383
Dharmik rabari <dharmik161616@gmail.com>
2026-04-25 16:36:50 +0530

    Add files via upload

---
+++ js-daily-Question/5.findTheLargestNum.js
@@ -0,0 +1,11 @@
+//largest number in array
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 24];
+
+function check1() {
+  let max = Math.max(...num);
+
+  console.log("largest number is : ",max);
+}
+
+check1();
